122bb9bdf130f49358d09c37b4256947df3d57bf
Júlio <tdsjsa2010@gmail.com>
2026-08-13 09:47:22 -0300

    Add files via upload

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity} from "react-native";
+
+
 
 export default function Home() {
   return (
-    <View>
-      <Text>INDEX</Text>
+    
+    <View style={s.container}>
+        <ImageBackground source={require('../assets/images/capa.png')} resizeMode="cover" style={s.image}>
+        <Image source={require('../assets/images/logo.png')} style={s.logo}>
+        <TouchableOpacity style={s.btn}>
+          <Text>ENTRAR</Text>
+        </TouchableOpacity>
+       </ImageBackground>
     </View>
   );
 }
-const s = StyleSheet.create({});
+const s = StyleSheet.create({
+    container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  btn:{
+
+  },
+  logo:{
+
+  },
+
+});
